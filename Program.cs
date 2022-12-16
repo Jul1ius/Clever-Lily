93c8b35541c0710f77d971504043850f5ccc01f5
its julii <nanji828@gmail.com>
2022-12-16 19:47:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Clever_Lily
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double washingMachinePrice = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+            double sum = 0;
+            int moneyRecieved = 10;
+
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += moneyRecieved;
+                    moneyRecieved += 10;
+                    sum--;
+                }
+                else
+                {
+                    sum += toyPrice;
+                }
+            }
+            if (sum < washingMachinePrice)
+            {
+                Console.WriteLine($"No! {washingMachinePrice - sum:f2} ");
+            }
+            else
+            {
+                Console.WriteLine($"Yes! {sum - washingMachinePrice:f2} ");
+            }
+        }
+
+    }
+}
+
